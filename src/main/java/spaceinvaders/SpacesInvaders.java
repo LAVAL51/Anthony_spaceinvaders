98d9e13b47671eb2eac7403e47f14f0ce21bb6a1
anthony <anthony.laval@etu.unilim.fr>
2021-04-27 18:43:02 +0200

    Story 4 : Déplacer le vaiseau vers la gauche

--- src/main/java/spaceinvaders/SpacesInvaders.java
+++ src/main/java/spaceinvaders/SpacesInvaders.java
@@ -62,5 +62,10 @@ public class SpacesInvaders {
 
 	   	public void deplacerVaisseauVersLaDroite() {
 	        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+		}
+
+		public void deplacerVaisseauVersLaGauche() {
+			if (vaisseau.abscisse()> 0) vaisseau.seDeplacerVersLaGauche();
+			
 		}	
 }
